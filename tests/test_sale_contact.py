528e24ff30890ab48890728cca36e2bf1ba0d26b
resteve <resteve@zikzakmedia.com>
2015-12-01 11:18:15 +0100

    Tests

--- tests/test_sale_contact.py
+++ tests/test_sale_contact.py
@@ -1,21 +1,14 @@
-#!/usr/bin/env python
-# This file is part sale_contact module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# This file is part of the sale_contact module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class SaleContactTestCase(unittest.TestCase):
+class SaleContactTestCase(ModuleTestCase):
     'Test Sale Contact module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('sale_contact')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'sale_contact'
 
 
 def suite():
